aa06c99a81cd905ed835129e4a8de6b943fe38a8
Fredyzxy <138654587+Fredyzxy@users.noreply.github.com>
2023-07-09 15:54:37 +0700

    Add files via upload

--- replit.nix
+++ replit.nix
@@ -1,8 +1,10 @@
 { pkgs }: {
-	deps = [
-		pkgs.nodejs-18_x
-    pkgs.nodePackages.typescript-language-server
-    pkgs.yarn
-    pkgs.replitPackages.jest
+	deps = with pkgs; [
+		nodejs-16_x
+		nodePackages.typescript-language-server
+		nodePackages.node-pre-gyp
+		libpng
+		libjpeg
+		libuuid
 	];
 }
